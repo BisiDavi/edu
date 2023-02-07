8a2f3ac30f47b2f2b03ee5e2ee89bd07abfb6b88
BisiDavi <oludavidconnect@gmail.com>
2023-02-07 22:43:27 +0100

    Menulink added

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,31 +1,17 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-
 import Button from "components/Button";
 import menuContent from "json/menu.json";
+import MenuLink from "components/MenuLink";
 
 export default function Menu() {
-	const router = useRouter();
-	const activeStyle = {
-		padding: "8px",
-		borderBottom: "3px solid black",
-	};
-
 	return (
 		<nav className="flex items-center">
 			<ul className="flex space-x-6">
-				{menuContent.map((item) => {
-					const styles = router.asPath.includes(item.link) ? activeStyle : {};
-					return (
-						<li key={item.text}>
-							<Link href={item.link} style={styles}>
-								{item.text}
-							</Link>
-						</li>
-					);
-				})}
+				<MenuLink menu={menuContent.link} />
 			</ul>
-			<Button className="mx-4 border rounded-full px-6 py-1.5" text="Login" />
+			<Button
+				className="mx-8 border rounded-full px-6 py-1.5 hover:bg-primary hover:text-white"
+				text="Login"
+			/>
 		</nav>
 	);
 }
